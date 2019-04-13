cb90965c3f855265aa94d953537fb5961ae83da7
Monroe <mgetz10@gmail.com>
2019-04-13 19:40:39 -0400

    heroku 2

--- server/app.js
+++ server/app.js
@@ -52,7 +52,6 @@ app.use(
 );
 
 app.use('/api', require('./routes/index'));
-app.use('/api', require('./routes/auth'));
 
 // For any routes that starts with "/api", catch 404 and forward to error handler
 app.use('/api/*', (req, res, next) => {
