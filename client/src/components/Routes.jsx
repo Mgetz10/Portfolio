7a87bb9ad0ce10388b83c569a700f5cae6a027d3
Monroe <mgetz10@gmail.com>
2019-04-15 23:19:37 -0400

    cleaned routes

--- client/src/components/Routes.jsx
+++ client/src/components/Routes.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 
+//fixed
 class Routes extends Component {
   render() {
     return (
